9c9d588e4d9ed4eded00e10aabb999631638385c
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-13 11:16:51 -0600

    Clearing form after submit

--- client.js
+++ client.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){ //Waits for DOM to load
-  $('#submitButton').on('click', function(){ //sets up an event listener on submit button
+
+  //sets up an event listener on submit button
+  $('#submitButton').on('click', function(){
+    //setting up input variables
     var firstName = $('#firstName').val();
     var lastName = $('#lastName').val();
     var idNumber = $('#idNumber').val();
     var jobTitle = $('#jobTitle').val();
     var annualSalary = $('#annualSalary').val();
+
     //Adds the employee data to the employee list
     $('#employeeTableBody').append(
       '<tr>' +
@@ -16,15 +20,19 @@ $(document).ready(function(){ //Waits for DOM to load
       '<td><button class ="deleteButton">Delete ' + firstName + '</buton></td>' +
       '</tr>'
     );//ends append
+
     //Add monthly salary expenses to the DOM;
     var newEmployee = annualSalary/12; //one employees salary / 12
     var previousTotal = $('#monthlyExpenses').text(); //finds the value already located in the monthly expenses ('0')
     var totalMonthlyExpenses = parseFloat(previousTotal) + newEmployee; //changes the original value in the monthly expenses to a number and adds that with the new employee
     $('#monthlyExpenses').text(totalMonthlyExpenses); //puts the previous var into the monthly expenses
+    $('.employeeFormInput').val(''); //clears out the input box
   });// ends the onclick function
+  
   //starts new event listener on delete click
   $('#employeeTableBody').on('click', '.deleteButton', function (){
-    $(this).parent().parent().remove();
+    $(this).parent().parent().remove(); //removes the row that the delete button is tied to;
+
   });
 
 
