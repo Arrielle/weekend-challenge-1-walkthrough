f748916879a752a3d3a08007d6f844a88adcde18
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-14 09:52:17 -0600

    converting to jquery form

--- client.js
+++ client.js
@@ -1,30 +1,49 @@
 $(document).ready(function(){ //Waits for DOM to load
+  // 'form' - WHAT TO LISTEN FOR 'on' WHEN IT'S LISTENED TO 'submit' - THE EVENT
+  //(SOMETHING COULD GO HERE FOR A REPLACEMENT OF FORM TO ATTACH THE EVENT TO) function - RUNS THE FUNCTION
+  $('form').on('submit', function(event){   //sets up an event listener on submit button
+    //DOES NOT BRING US TO A NEW PAGE (THE DEFAULT EVENT OF THE SUBMIT EVENT
+    event.preventDefault();
+    //SERIALIZEARRAY CREATES AN ARRAY OF THE INPUTS, THE INPUTS ARE CONVERTED TO OBJECTS
+    //THE OBJECTS HAVE TWO PROPERTIES, NAME AND VALUE
+    var submissionArray = $(this).serializeArray(); //GIVES OUR ARRAY A VARIABLE
+    //ARRAY IS NOT USEFULT YET. IT'S AN ARRAY OF OBJECTS FOR EACH EMPLOYEE
+    //WE WANT TO TURN EACH EMPLOYEE INTO ITS OWN OBJECT
+    var newEmployeeObject = {}; //EMPTY OBJECTS
+    submissionArray.forEach(function(inputFieldObject){
+      //FOR EACH NEW EMPLOYEE OBJECT
+      //FIRST TIME INPUT FIELD IS AN OBJECT WITH TWO PROPERTIES, NAME AND VALUE
+      //FIRST TIME TRHOUGH NEWEMPLOYEE OBJECT IS EMPTY.
+      //ONCE IT'S RUN THROUGH, NEW EMPLOYEEOBJECT THE INPUTFIELD.NAME (firstName) = INPUTFIELD.VALUE (value(Luke,Arri,Paul,etc))
+      // newEmployeeObject.firstName = name (luke);
+      //FIRST TIME THROUHG {firstName: Luke}
+      //SECOND TIME THROUGH {firstName: Luke, secondName: Schlugnun}
+      newEmployeeObject[inputFieldObject.name] = inputFieldObject.value;
+    }); //ends for each loop
 
-  //sets up an event listener on submit button
-  $('#submitButton').on('click', function(){
-    //setting up input variables
-    var firstName = $('#firstName').val();
-    var lastName = $('#lastName').val();
-    var idNumber = $('#idNumber').val();
-    var jobTitle = $('#jobTitle').val();
-    var annualSalary = $('#annualSalary').val();
+    //above code as a for loop
+    // for (var i = 0; i < submissionArray.length; i++) {
+    //   var inputFieldObject = submissionArray[i];
+    //   newEmployeeObject[inputFieldObject.name] = inputFieldObject.value;
+    // } //ends for loop
 
-    //Adds the employee data to the employee list
+    console.log(newEmployeeObject);
+
     $('#employeeTableBody').append(
       '<tr>' +
-      '<td>' + firstName + '</td>' +
-      '<td>' + lastName + '</td>' +
-      '<td>' + idNumber + '</td>' +
-      '<td>' + jobTitle + '</td>' +
-      '<td>' + annualSalary + '</td>' +
-      '<td><button class ="deleteButton" data-salary = "' + annualSalary +'" >Delete ' + firstName + '</buton></td>' +
+      '<td>' + newEmployeeObject.firstName + '</td>' +
+      '<td>' + newEmployeeObject.lastName + '</td>' +
+      '<td>' + newEmployeeObject.idNumber + '</td>' +
+      '<td>' + newEmployeeObject.jobTitle + '</td>' +
+      '<td>' + newEmployeeObject.annualSalary + '</td>' +
+      '<td><button class ="deleteButton" data-salary = "' + newEmployeeObject.annualSalary +'" >Delete ' + newEmployeeObject.firstName + '</buton></td>' +
       '</tr>'
     );//ends append
 
     //Add monthly salary expenses to the DOM;
-    var newEmployee = annualSalary/12; //one employees salary / 12
+    var newEmployeeSalary = newEmployeeObject.annualSalary/12; //one employees salary / 12
     var previousTotal = $('#monthlyExpenses').text(); //finds the value already located in the monthly expenses ('0')
-    var totalMonthlyExpenses = parseFloat(previousTotal) + newEmployee; //changes the original value in the monthly expenses to a number and adds that with the new employee
+    var totalMonthlyExpenses = parseFloat(previousTotal) + newEmployeeSalary; //changes the original value in the monthly expenses to a number and adds that with the new employee
     $('#monthlyExpenses').text(totalMonthlyExpenses); //puts the previous var into the monthly expenses
     $('.employeeFormInput').val(''); //clears out the input box
   });// ends the onclick function
